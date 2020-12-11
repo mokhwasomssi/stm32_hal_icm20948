97152b731c6ce574387732a584dd22f48f1849d3
석은혜 <48342925+mokhwasomssi@users.noreply.github.com>
2020-12-11 21:50:48 +0900

    Update ICM20948.h

--- ICM20948.h
+++ ICM20948.h
@@ -38,13 +38,67 @@ typedef struct _ICM20948_DATA
 	int16_t Mag_X_Data;			// Magnetometor Output Data
 	int16_t Mag_Y_Data;
 	int16_t Mag_Z_Data;
-
 } ICM20948_DATA;
-/* ICM20948 Data Structure */
 
 
-/* typedef Register Setting */
-typedef enum _USER_CTRL				/* ICM-20948 DataSheet Page 36/89 */
+/* UserBank */
+typedef enum _UserBank
+{
+	UsarBank_0,
+	UserBank_1,
+	UserBank_2,
+	UserBank_3
+} UserBank;
+
+
+/* Initialize ICM-20948(Gyroscope, accelerometer) */
+typedef enum _PWR_MGNT_1			// Reset Value : 0x41
+{
+	DEVICE_RESET 	= 1 << 7,		// Reset the internal registers and restores the default settings.
+	SLEEP 			= 1 << 6,		// When set, the chip is set to sleep mode
+	WAKE			= 0 << 6,		// Clearing the bit wakes the chip from sleep mode.
+	LP_EN 			= 1 << 5,
+	TEMP_DIS		= 1 << 3,
+	CLKSEL 			= 1
+} PWR_MGNT_1;
+
+typedef enum _GYRO_SMPLRT_DIV
+{
+	Gyro_ODR_1100Hz = 0,			// Output Data Rate = 1.1kHz / (1 + GYRO_SMPLRT_DIV)
+	Gyro_ODR_100Hz = 10,
+	Gyro_ODR_10Hz = 109
+} GYRO_SMPLRT_DIV;
+
+typedef enum _GYRO_CONFIG_1			// Reset Value : 0x01
+{
+	GYRO_DLPFCFG = 0,				// Gyro low pass filter configuration (Table 16)
+	GYRO_FS_SEL_250dps = 1,			// Gyro Full Scale Select
+	GYRO_FS_SEL_500dps = 3,
+	GYRO_FS_SEL_1000dps = 5,
+	GYRO_FS_SEL_2000dps = 7,
+	GYRO_FCHOICE = 1				// Enable gyro DLPF
+} GYRO_CONFIG_1;
+
+typedef enum _ACCEL_SMPLRT_DIV_2
+{
+	Accel_ODR_1100Hz = 0,			// Output Data Rate = 1.1kHz / (1 + ACCEL_SMPLRT_DIV)
+	Accel_ODR_100Hz = 10,
+	Accel_ODR_10Hz = 109
+} ACCEL_SMPLRT_DIV_2;
+
+typedef enum _ACCEL_CONFIG			// Reset Value : 0x01
+{
+	ACCEL_DLPFCFG = 0,				// accel low pass filter configuration (Table 18)
+	ACCEL_FS_SEL_2g = 1,
+	ACCEL_FS_SEL_4g = 3,
+	ACCEL_FS_SEL_8g = 5,
+	ACCEL_FS_SEL_16g = 7,
+	ACCEL_FCHOICE = 1				// Enable accel DLPF
+} ACCEL_CONFIG;
+
+
+/* Initialize AK09916(Magnetometer) */
+typedef enum _USER_CTRL
 {
 	DMP_EN 			= 1 << 7,		// Enables DMP features
 	FIFO_EN 		= 1 << 6,		// Enable FIFO operation mode
@@ -54,23 +108,31 @@ typedef enum _USER_CTRL				/* ICM-20948 DataSheet Page 36/89 */
 	SRAM_RST 		= 1 << 2,		// Reset SRAM module
 	I2C_MST_RST		= 1 << 1		// Reset I2C Master module
 } USER_CTRL;
-/* typedef Register Setting */
 
-
-/* typedef Register Value of I2C */
-typedef enum _I2C_MST_ODR_CONFIG			/* ICM-20948 DataSheet Page 68/89 */
+typedef enum _I2C_MST_ODR_CONFIG
 {
 	I2C_MST_ODR_CONFIG = 0					// ODR configuration for external sensor when gyroscope and accelerometer are disabled.
 											// ODR = 1.1kHz / ( 2 ^(odr_config[3:0]) )
-} I2C_MST_ODR_CONFIG;
+} _I2C_MST_ODR_CONFIG;
 
 typedef enum _I2C_MST_CTRL
 {
-
+	MULT_MST_EN				= 1 << 7,				// Enables multi-master capability
+	I2C_MST_P_NSR			= 1 << 4,				// This bit controls the I2C Master's transition
+	I2C_MST_CLK				= 0						// Sets I2C master clock frequency (Table 23)
 } I2C_MST_CTRL;
 
+typedef enum _I2C_MST_DELAY_CTRL
+{
+	DELAY_ES_SHADOW			= 1 << 7,				// Delays shadowing of external sensor data until all data is received
+	I2C_SLV4_DELAY_EN 		= 1 << 4,				// When enabled, slave 0-4 will only be accessed 1/(1+I2C_SLC4_DLY) samples
+	I2C_SLV3_DELAY_EN 		= 1 << 3,				// as determined by I2C_MST_ODR_CONFIG
+	I2C_SLV2_DELAY_EN 		= 1 << 2,
+	I2C_SLV1_DELAY_EN 		= 1 << 1,
+	I2C_SLV0_DELAY_EN 		= 1 << 0
+} I2C_MST_DELAY_CTRL;
 
-typedef enum _I2C_SLV_CTRL					/* ICM-20948 DataSheet Page 70/89 */
+typedef enum _I2C_SLV_CTRL
 {
 	I2C_SLV_EN				= 1 << 7,		// Enable reading data from this slave.
 	I2C_SLV_BYTE_SW 		= 1 << 6,		// Swap bytes when reading both the low and high byte of a word.
@@ -78,51 +140,6 @@ typedef enum _I2C_SLV_CTRL					/* ICM-20948 DataSheet Page 70/89 */
 	I2C_SLV_GRP 			= 1 << 4,		// External sensor data typically comes in as groups of two bytes
 	I2C_SLV_LENG_6bytes 	= 6				// Number of bytes to be read from I2C slave
 } I2C_SLV_CTRL;
-/* typedef Register Value of I2C */
-
-
-
-/* typedef ICM-20948 Register Value */
-typedef enum _UserBank
-{
-	UsarBank_0,
-	UserBank_1,
-	UserBank_2,
-	UserBank_3
-} UserBank;
-
-typedef enum _GyroOutDataRate
-{
-	Gyro_ODR_1100Hz = 0,
-	Gyro_ODR_100Hz = 10,
-	Gyro_ODR_10Hz = 109
-} Gyro_ODR;
-
-typedef enum _AccelOutDataRate
-{
-	Accel_ODR_1100Hz = 0,
-	Accel_ODR_100Hz = 10,
-	Accel_ODR_10Hz = 109
-} Accel_ODR;
-
-typedef enum _GyroFullScaleSellet
-{
-	Gyro_Scale_250dp = 1,
-	Gyro_Scale_500dps = 3,
-	Gyro_Scale_1000dps = 5,
-	Gyro_Scale_2000dps = 7
-} Gyro_Scale;
-
-typedef enum _AccelFullScaleSellet
-{
-	Accel_Scale_2g = 1,
-	Accel_Scale_4g = 3,
-	Accel_Scale_8g = 5,
-	Accel_Scale_16g = 7
-} Accel_Scale;
-/* typedef ICM-20948 Register Value */
-
-
 
 
 
@@ -135,11 +152,42 @@ void WRITE_REGISTER(uint8_t RegisterAddress, uint8_t WriteData);
 
 uint8_t WHOAMI();
 void SELECT_USER_BANK(UserBank UB);
-void INIT_ICM20948(Gyro_ODR godr, Accel_ODR aodr, Gyro_Scale gs, Accel_Scale as);
+void INIT_ICM20948();
+void INIT_AK09916();
 
 void READ_GYRO(ICM20948_DATA* myData);
 void READ_ACCEL(ICM20948_DATA* myData);
-/* functions */
+void READ_MAG(ICM20948_DATA* myData);
 
 
 #endif	// __MOKHWA_ICM20948_H__
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
